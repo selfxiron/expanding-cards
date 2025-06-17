5fd5826a63b0c5e57e05b430d0c1181082d91db6
selfxiron <selfxiron@gmail.com>
2025-06-18 01:29:50 +0530

    initial commit: expanding-cards

---
+++ index.js
@@ -0,0 +1,14 @@
+var panels = document.querySelectorAll('.panel');
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active');
+    });
+}
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses();
+        panel.classList.add('active');
+    })
+});
